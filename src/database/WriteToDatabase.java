e520f10e5d5836a01f1c193f30c3e9f0b77aefe1
tazzaboy234 <43050523+Bandy99@users.noreply.github.com>
2019-03-26 14:26:15 +0000

    workingDB
    database now functional (you can connect to it). code wont run until the table is created. PLEASE DONT PUSH CODE IF CHANGES HAVE BEEN MADE TO THE DB.

--- src/database/WriteToDatabase.java
+++ src/database/WriteToDatabase.java
@@ -1,45 +1,111 @@
-import java.sql.*;
+package database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSetMetaData;
 
-public class WriteToDatabase 
-{
-   static final String JDBC_DRIVER = "sun.jdbc.odbc.JdbcOdbcDriver";  
-   static final String DB_URL = "/myfiles/awpk21/dos/ActiviTrakDB.accdb.mdb";
 
-   public static void main(String[] args) 
-   {
-	   Connection conn = null;
-	   Statement stmt = null;
-   
-	   try
-	   {
-	      Class.forName(JDBC_DRIVER);
-	
-	      System.out.println("Connecting to a selected database...");
-	      
-	      conn = DriverManager.getConnection(DB_URL);
-	      
-	      System.out.println("Connected database successfully...");
-	      
-	      stmt = conn.createStatement();
-	      
-	      String sql = "INSERT INTO Registration " + "VALUES ('Hazza', 'Harry', 21, M, 15012, 4005, 6)";
-	      stmt.executeUpdate(sql);
-	      
-	      System.out.println("Inserted record into the table...");
-	
-	   }
-	   
-	   catch(SQLException se)
-	   {
-	      System.out.print("SQL Fault!\n");
-	   }
-	   
-	   catch(Exception e)
-	   {
-	      System.out.print("Exception Fault!\n");
-	   }
-	   
-	   System.out.println("Goodbye!");
-   
-   }
+public class WriteToDatabase {
+    private static String dbURL = "jdbc:derby:C:\\Users\\tband\\OneDrive\\Documents\\GitHub\\CSED-2\\src\\database\\newDerby;create=false";
+    private static String tableName = "workouts";
+    // jdbc Connection
+    private static Connection conn = null;
+    private static Statement stmt = null;
+
+    public static void main(String[] args) {
+        createConnection();
+        createTable("workouts");
+        insertData(5, "LaVals", "Berkeley");
+        selectData();
+        shutdown();
+    }
+    
+    private static void createTable(String tableName) {
+    	try { 
+    		stmt = conn.createStatement();
+    		stmt.execute("CREATE TABLE " + tableName + " column1 datatype, column2 datatype, column3 datatype");
+    	
+    	} catch (SQLException se) {
+    		se.printStackTrace();
+    	}
+    }
+    
+    
+    
+    private static void createConnection() {
+        try {
+            //Get a connection
+            conn = DriverManager.getConnection(dbURL); 
+        }
+        catch (Exception except) {
+            except.printStackTrace();
+        }
+    }
+    
+    private static void insertData(int id, String restName, String cityName) {
+        try {
+            stmt = conn.createStatement();
+            stmt.execute("insert into " + tableName + " values (" +
+                    id + ",'" + restName + "','" + cityName +"')");
+            stmt.close();
+        }
+        catch (SQLException sqlExcept) {
+            sqlExcept.printStackTrace();
+        }
+    }
+    
+    private static void selectData()
+    {
+        try
+        {
+            stmt = conn.createStatement();
+            ResultSet results = stmt.executeQuery("select * from " + tableName);
+            ResultSetMetaData rsmd = results.getMetaData();
+            int numberCols = rsmd.getColumnCount();
+            for (int i=1; i<=numberCols; i++)
+            {
+                //print Column Names
+                System.out.print(rsmd.getColumnLabel(i)+"\t\t");  
+            }
+
+            System.out.println("\n-------------------------------------------------");
+
+            while(results.next())
+            {
+                int id = results.getInt(1);
+                String restName = results.getString(2);
+                String cityName = results.getString(3);
+                System.out.println(id + "\t\t" + restName + "\t\t" + cityName);
+            }
+            results.close();
+            stmt.close();
+        }
+        catch (SQLException sqlExcept)
+        {
+            sqlExcept.printStackTrace();
+        }
+    }
+    
+    private static void shutdown()
+    {
+        try
+        {
+            if (stmt != null)
+            {
+                stmt.close();
+            }
+            if (conn != null)
+            {
+                DriverManager.getConnection(dbURL + ";shutdown=true");
+                conn.close();
+            }           
+        }
+        catch (SQLException sqlExcept)
+        {
+            
+        }
+
+    }
 }
