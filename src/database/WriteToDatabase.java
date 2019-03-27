07ca167293ca624ec067c5625b0634e73cf39d62
Aazzaboy234 <47715960+Aazzaboy234@users.noreply.github.com>
2019-03-27 22:19:18 +0000

    Update WriteToDatabase.java

--- src/database/WriteToDatabase.java
+++ src/database/WriteToDatabase.java
@@ -13,14 +13,17 @@ public class WriteToDatabase {
     private static Connection conn = null;
     private static Statement stmt = null;
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         createConnection();
-        createUserTable();
-//        insertData("USERS", "tazzaboy234", "Tom Bandurka", 19, 'M', 15000, 3000, 2000);
-//        selectData();
+        //createUserTable();
+
+        insertData("USERS", "tazzaboy234", "Tom Bandurka", 19, 'M', 15000, 3000, 2000);
+        selectData();
+
         shutdown();
     }
-    
+
+    // Table should be created - Call it "USERS"
     private static void createUserTable() {
     	try { 
     		stmt = conn.createStatement();	//this code creates a table with the data originally stored in a text file in v1.
@@ -44,7 +47,8 @@ public class WriteToDatabase {
     private static void insertData(String tableName, String user, String name, int age, char gender, int steps, int calories, int water) {
         try {
             stmt = conn.createStatement();
-            stmt.execute(""/*//TODO INSERT DATA SYNTAX HERE*/);
+            //stmt.execute(""/*//TODO INSERT DATA SYNTAX HERE*/);
+            stmt.executeUpdate("INSERT INTO USERS " + "VALUES (" + user + "," + name +"," + age + "," + gender + "," + steps + "," + calorie + "," + water +")");
             stmt.close();
         }
         catch (SQLException sqlExcept) {
@@ -52,7 +56,21 @@ public class WriteToDatabase {
         }
     }
     
-    private static void selectData() throws SQLException { 		//TODO reimplement this with a try/catch rather than a throws.
+    private static void selectData()
+    {
+        try {
+            //Example
+            ResultSet rs = stmt.executeQuery("SELECT Name FROM USERS WHERE userName = 'tazzaboy234'");
+
+            while (rs.next()) {
+                String name = rs.getstring("Name");
+                System.out.println(name + "\n");
+            }
+        }
+
+        catch (SQLException e) {
+            System.err.print("Unable to execute!");
+        }
     }
     
     private static void shutdown() {
